83421e9b92a7837808b3f9aeae27cddb73cb88dd
Can Ivit <canivit@users.noreply.github.com>
2022-12-27 18:45:39 +0300

    read config file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,49 @@
 use clap::Parser;
+use directories::ProjectDirs;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 pub struct Args {
     file: PathBuf,
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    from_email: String,
+    to_email: String,
+}
+
+pub fn run(args: &Args) -> Result<(), Box<dyn Error>> {
+    let config = read_config()?;
+    Ok(())
+}
+
+fn read_config() -> Result<Config, Box<dyn Error>> {
+    let config_file_path = ProjectDirs::from("canivit", "canivit", "skindle")
+        .ok_or("Failed to retrieve a valid home directory path")?
+        .config_dir()
+        .join("config.toml");
+
+    let content = fs::read_to_string(&config_file_path).map_err(|e| {
+        format!(
+            "Failed to read the config file {}: {}",
+            config_file_path.display(),
+            e
+        )
+    })?;
+
+    let config: Config = toml::from_str(&content).map_err(|e| {
+        format!(
+            "Failed to parse the config file {}: {}",
+            config_file_path.display(),
+            e
+        )
+    })?;
+
+    Ok(config)
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use clap::Parser;
 use skindle::Args;
+use std::process;
 
 fn main() {
-  let args = Args::parse();
+    let args = Args::parse();
+    if let Err(e) = skindle::run(&args) {
+        eprintln!("{e}");
+        process::exit(1);
+    }
 }
