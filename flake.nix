86c5a6ecc143885a8b178f15d759b97b8c678ddf
Can Ivit <canivit@users.noreply.github.com>
2023-02-03 21:20:33 -0500

    update flake to support all systems

--- flake.nix
+++ flake.nix
@@ -1,30 +1,24 @@
 {
   description = "skindle";
 
-  inputs = { 
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
-
-  in {
-    devShell.x86_64-linux = pkgs.mkShell {
-      name = "skindle";
-      buildInputs = with pkgs; [
-        cargo
-        rustc
-        rust-analyzer
-        rustfmt
-
-        # for lettre crate:
-        #openssl
-        #pkg-config
-      ];
-      shellHook = ''
-      '';
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        name = "skindle";
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rust-analyzer
+          rustfmt
+        ];
+      };
+    });
 }
